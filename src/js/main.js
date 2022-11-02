55a441c136850c09531ef0f91300754b5db098b6
SIDs1de <igorekdenisenkov@yandex.ru>
2022-11-02 11:45:39 +0300

    закончил первую страницу без адаптива

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+  VK.Widgets.Group(
+    'vk_groups',
+    {
+      mode: 3,
+      width: 541,
+      height: 204,
+      color1: 'FFFFFF',
+      color2: '000000',
+      color3: '5181B8',
+    },
+    171493284
+  );
 });
