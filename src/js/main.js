4ae0bc7900f6b86e7edd35948c6bb8cc6d4bb6c9
SIDs1de <igorekdenisenkov@yandex.ru>
2022-11-03 09:57:50 +0300

    исправил баги и улучшил адаптив

--- src/js/main.js
+++ src/js/main.js
@@ -1,57 +1,60 @@
 document.addEventListener('DOMContentLoaded', () => {
   const vkSwitchWidth = () => {
-    if (window.innerWidth < 370) {
-      VK.Widgets.Group(
-        'vk_groups',
-        {
-          mode: 3,
-          width: 270,
-          height: 204,
-          color1: 'FFFFFF',
-          color2: '000000',
-          color3: '5181B8',
-        },
-        171493284
-      );
-    } else if (window.innerWidth < 420) {
-      VK.Widgets.Group(
-        'vk_groups',
-        {
-          mode: 3,
-          width: 320,
-          height: 204,
-          color1: 'FFFFFF',
-          color2: '000000',
-          color3: '5181B8',
-        },
-        171493284
-      );
-    } else if (window.innerWidth < 600) {
-      VK.Widgets.Group(
-        'vk_groups',
-        {
-          mode: 3,
-          width: 370,
-          height: 204,
-          color1: 'FFFFFF',
-          color2: '000000',
-          color3: '5181B8',
-        },
-        171493284
-      );
-    } else {
-      VK.Widgets.Group(
-        'vk_groups',
-        {
-          mode: 3,
-          width: 541,
-          height: 204,
-          color1: 'FFFFFF',
-          color2: '000000',
-          color3: '5181B8',
-        },
-        171493284
-      );
+    const vk_groups = document.querySelector('.buy__vk-groups');
+    if (vk_groups) {
+      if (window.innerWidth < 370) {
+        VK.Widgets.Group(
+          'vk_groups',
+          {
+            mode: 3,
+            width: 270,
+            height: 204,
+            color1: 'FFFFFF',
+            color2: '000000',
+            color3: '5181B8',
+          },
+          171493284
+        );
+      } else if (window.innerWidth < 420) {
+        VK.Widgets.Group(
+          'vk_groups',
+          {
+            mode: 3,
+            width: 320,
+            height: 204,
+            color1: 'FFFFFF',
+            color2: '000000',
+            color3: '5181B8',
+          },
+          171493284
+        );
+      } else if (window.innerWidth < 600) {
+        VK.Widgets.Group(
+          'vk_groups',
+          {
+            mode: 3,
+            width: 370,
+            height: 204,
+            color1: 'FFFFFF',
+            color2: '000000',
+            color3: '5181B8',
+          },
+          171493284
+        );
+      } else {
+        VK.Widgets.Group(
+          'vk_groups',
+          {
+            mode: 3,
+            width: 541,
+            height: 204,
+            color1: 'FFFFFF',
+            color2: '000000',
+            color3: '5181B8',
+          },
+          171493284
+        );
+      }
     }
   };
 
@@ -59,19 +62,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const selectWrapper = document.querySelector('.buy__select-wrapper');
     const select = document.querySelector('.buy__select');
 
-    select.addEventListener('click', () => {
-      if (selectWrapper.classList.contains('_open')) {
+    if (select) {
+      select.addEventListener('click', () => {
+        if (selectWrapper.classList.contains('_open')) {
+          selectWrapper.classList.remove('_open');
+        } else {
+          selectWrapper.classList.add('_open');
+        }
+      });
+      select.addEventListener('blur', () => {
         selectWrapper.classList.remove('_open');
-      } else {
-        selectWrapper.classList.add('_open');
-      }
-    });
-    select.addEventListener('blur', () => {
-      selectWrapper.classList.remove('_open');
-    });
-    select.addEventListener('changed', () => {
-      selectWrapper.classList.remove('_open');
-    });
+      });
+      select.addEventListener('changed', () => {
+        selectWrapper.classList.remove('_open');
+      });
+    }
   };
 
   vkSwitchWidth();
