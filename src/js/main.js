d849325f0d35b9a9d3646a782e5c362f0c66f6d3
SIDs1de <igorekdenisenkov@yandex.ru>
2022-11-03 12:10:37 +0300

    исправил баги, сделал анимации и модальные окна

--- src/js/main.js
+++ src/js/main.js
@@ -79,6 +79,61 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
 
+  const popupsScript = () => {
+    const openPopupsBtns = document.querySelectorAll('[data-open-popup]');
+    const popups = document.querySelectorAll('[data-popup]');
+    const html = document.querySelector('html');
+    if (popups) {
+      openPopupsBtns.forEach((btn) => {
+        btn.addEventListener('click', () => {
+          event.preventDefault();
+          popups.forEach((popup) => {
+            if (popup.dataset.popup === btn.dataset.openPopup) {
+              popup.classList.add('active');
+              const closeBtn = popup.querySelector('.popup__inner-close');
+              const back = popup.querySelector('.popup__bg');
+              html.classList.add('lock');
+
+              closeBtn.addEventListener('click', () => {
+                popup.classList.remove('active');
+                html.classList.remove('lock');
+              });
+
+              back.addEventListener('click', () => {
+                popup.classList.remove('active');
+                html.classList.remove('lock');
+              });
+
+              const popupBody = popup.querySelector('.popup__body');
+              const popupInner = popup.querySelector('.popup__inner');
+              popupBody.addEventListener('click', (e) => {
+                if (e.target === popupInner || e.target === popupBody) {
+                  popup.classList.remove('active');
+                  html.classList.remove('lock');
+                }
+              });
+            }
+          });
+        });
+      });
+    }
+  };
+
+  new WOW({
+    boxClass: 'wow', // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset: 0, // distance to the element when triggering the animation (default is 0)
+    mobile: true, // trigger animations on mobile devices (default is true)
+    live: true, // act on asynchronously loaded content (default is true)
+    callback: function (box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null, // optional scroll container selector, otherwise use window,
+    resetAnimation: true, // reset animation on end (default is true)
+  }).init();
+
   vkSwitchWidth();
   selectSwitch();
+  popupsScript();
 });
