6fbf70a803c0d8693bc377ef40548bd9877c5f87
SIDs1de <igorekdenisenkov@yandex.ru>
2023-02-09 16:54:14 +0300

    добавил платёжную систему

--- src/js/main.js
+++ src/js/main.js
@@ -189,7 +189,33 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       }
       if (!flag) {
-        form.submit();
+        var widget = new cp.CloudPayments();
+        widget.pay('auth', // или 'charge'
+          { //options
+            publicId: 'test_api_00000000000000000000002', //id из личного кабинета
+            description: 'Оплата товаров в example.com', //назначение
+            amount: 100, //сумма
+            currency: 'RUB', //валюта
+            accountId: 'user@example.com', //идентификатор плательщика (необязательно)
+            invoiceId: '1234567', //номер заказа  (необязательно)
+            email: 'user@example.com', //email плательщика (необязательно)
+            skin: "mini", //дизайн виджета (необязательно)
+            autoClose: 3, //время в секундах до авто-закрытия виджета (необязательный)
+            data: {
+              myProp: 'myProp value'
+            }
+          },
+          {
+            onSuccess: function (options) { // success
+              //действие при успешной оплате
+            },
+            onFail: function (reason, options) { // fail
+              //действие при неуспешной оплате
+            },
+            onComplete: function (paymentResult, options) { //Вызывается как только виджет получает от api.cloudpayments ответ с результатом транзакции.
+              //например вызов вашей аналитики Facebook Pixel
+            }
+          })
       }
     });
   };
